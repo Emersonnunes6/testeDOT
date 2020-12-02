5d8169c35d184f3bccad260c0de240a553375660
unknown <enunessena@gmail.com>
2020-12-02 17:49:58 -0300

    testedot-1

---
+++ slider.js
@@ -0,0 +1,5 @@
+function mostrar(id) {
+    if(document.getElementById(id).style.display == "none"){
+        document.getElementById(id).style.display = "flex";
+}else {(document.getElementById(id).style.display = "none")}
+}
